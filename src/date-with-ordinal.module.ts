d9ee55c757066ad4132a95bd3fd1d7b76033f640
Jay Vincent <jay.vincent@wonga.com>
2018-06-04 13:56:46 +0100

    Updates devDependencies to Angular 6, adds CommonModule import to feature module
    Signed-off-by: Jay Vincent <jay.vincent@wonga.com>

--- src/date-with-ordinal.module.ts
+++ src/date-with-ordinal.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DateWithOrdinalPipe } from './date-with-ordinal.pipe';
 
 @NgModule({
+  imports: [
+    CommonModule
+  ],
   declarations: [
     DateWithOrdinalPipe
   ],
